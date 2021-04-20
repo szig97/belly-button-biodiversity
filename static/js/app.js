211c0e51827040bc75f9e3aa237d0935e840ef6f
Sam Ziegler <szig97@outlook.com>
2021-04-20 13:52:35 -0500

    moved samples.json to data folder and added populating dropdown code

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,16 @@
 // PORTIONS OF THIS CODE ARE TAKEN FROM INSTRUCTOR TUTORIAL
+
 console.log("app load success")
 
 function InitDashboard() {
+    console.log("InitDashboard()");
 
     // Populate dropdown
+    var selector = d3.select("selDataset");
+
+    d3.json("data/samples.json").then(function(data) {
+        console.log(data);
+    });
 
     // Update bargraph
 
