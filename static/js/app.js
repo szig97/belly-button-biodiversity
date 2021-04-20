b5b9171d5319ec5e1e1b3b59c58c0918fa78147e
Sam Ziegler <szig97@outlook.com>
2021-04-20 18:03:11 -0500

    adding bubblegraph code

--- static/js/app.js
+++ static/js/app.js
@@ -19,7 +19,7 @@ function DrawBargraph(sampleId) {
 
         yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
 
-        var barData = {
+        var barTrace = {
             x: sample_values.slice(0,10).reverse(),
             y: yticks,
             type: "bar",
@@ -27,19 +27,50 @@ function DrawBargraph(sampleId) {
             orientation: "h"
         }
 
-        var barArray = [barData];
+        var barArray = [barTrace];
 
         var barLayout = {
             title: "Top 10 Bacteria Cultures Found",
             margin: {t: 30, l: 150}
         }
-        Plotly.newPlot("bar", barArray, barLayout)
+        Plotly.newPlot("bar", barArray, barLayout);
     });
 }
 
 // Draw Bubblechart (Event Handler)
 function DrawBubblechart(sampleId) {
     console.log(`Draw Bubblechart(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        var bubbleTrace = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+            }
+        }
+    
+        var bubbleArray = [bubbleTrace];
+    
+        var bubbleLayout = {
+            margin: { t: 0 },
+            xaxis: {title: "OTU ID"}
+        };
+        Plotly.newPlot("bubble", bubbleArray, bubbleLayout);  
+    });  
 }
 
 // Show Metadata (Event Handler)
