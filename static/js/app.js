721a4dbb480a316c8c9edd34bf3e398c962fca04
Sam Ziegler <szig97@outlook.com>
2021-04-20 13:43:37 -0500

    adding initial code

---
+++ static/js/app.js
@@ -0,0 +1,15 @@
+// PORTIONS OF THIS CODE ARE TAKEN FROM INSTRUCTOR TUTORIAL
+console.log("app load success")
+
+function InitDashboard() {
+
+    // Populate dropdown
+
+    // Update bargraph
+
+    // Update bubblechart
+
+    // Update demographics
+}
+
+InitDashboard();
