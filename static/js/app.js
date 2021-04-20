0b027d75eb737b0ca46f20cba63ee30fbf1a414e
Sam Ziegler <szig97@outlook.com>
2021-04-20 15:18:21 -0500

    drawing bar graph code

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,36 @@ console.log("app load success")
 // Draw bargraph (Event Handler)
 function DrawBargraph(sampleId) {
     console.log(`Draw Bargraph(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0,10).reverse(),
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+        Plotly.newPlot("bar", barArray, barLayout)
+    });
 }
 
 // Draw Bubblechart (Event Handler)
@@ -46,7 +76,7 @@ function InitDashboard() {
 
         var id = sampleNames[0];
 
-        //Draw Graphs and Metadata
+        // Draw Graphs and Metadata
         DrawBargraph(id);
         DrawBubblechart(id);
         ShowMetadata(id);
